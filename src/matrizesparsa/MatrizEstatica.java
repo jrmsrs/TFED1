86c320c735f99baa212e7e376472d9ecc4cc4ff0
Jr. M. Srs <arlsjunior@edu.unirio.br>
2022-02-05 17:05:01 -0300

    att em MatrizEstatica

--- src/matrizesparsa/MatrizEstatica.java
+++ src/matrizesparsa/MatrizEstatica.java
@@ -24,36 +24,97 @@ public class MatrizEstatica {
     
     //3. Busca por um elemento específico;
     public boolean busca(int buscaDado) {
+        for (int i = 0; i < this.lin; i++) 
+        for (int j = 0; j < this.col; j++) 
+            if (matriz[i][j] == buscaDado) 
+                return true;
         return false;
     }
-    
+
     //4. Impressão da matriz;
     public void imprime() {
+        for (int i = 0; i < this.lin; i++) {
+            System.out.println();
+            for (int j = 0; j < this.col; j++)
+                System.out.print("  " + matriz[i][j] + "  ");
+            System.out.println();
+        }
+        System.out.println();
     }
 
     //5. Representar uma matriz vazia;
     public MatrizEstatica(int l, int c) {
-        //preenche tudo com 0
+        this.matriz = new int[l][c];
+        this.lin = l;
+        this.col = c;
+        for (int i = 0; i < lin; i++) 
+        for (int j = 0; j < col; j++) 
+            this.matriz[i][j] = 0;
     }
-    
+
     //6. Verificar se é uma matriz vazia;
     public boolean isVazia() {
-        return false;
+        if (matriz == null) return true;
+        for (int i = 0; i < lin; i++) 
+        for (int j = 0; j < col; j++) 
+            if (matriz[i][j] != 0) 
+                return false;
+        return true;
     }
 
     //7. Verificar se é uma matriz diagonal (só tem elementos na diagonal principal);
     public boolean isDiagonal() {
-        return false;
+        if (this.isVazia()) return false;
+
+        //varre a matriz
+        for (int i = 0; i < this.lin; i++) {
+            for (int j = 0; j < this.col; j++) {
+                if (i != j && matriz[i][j] != 0) {
+                    //se encontrar algum valor quando i != j, ou seja, fora da diagonal principal
+                    return false;
+                }
+            }
+        }
+        //caso contrario:
+        return true;
     }
 
     //8. Verificar se é uma matriz linha (só tem elementos em uma única linha);
     public boolean isLinha() {
-        return false;
+        if (this.isVazia()) return false;
+        //se nao esta vazia pelo menos uma linha ja contem elemento(s)
+        if (this.lin == 1) return true;
+        //nao precisa percorrer em O(n*m) a matriz se ela tiver apenas uma linha
+
+        int contador = 0;
+
+        for (int i = 0; i < this.lin; i++)
+        for (int j = 0; j < this.col; j++) 
+            if (matriz[i][j] != 0) {
+                contador++;
+                if (contador>1) return false;
+                break; //pula pra proxima linha
+            }
+        
+        return true;
     }
 
     //9. Verificar se é uma matriz coluna (só tem elementos em uma única coluna);
     public boolean isColuna() {
-        return false;
+        if (this.isVazia()) return false;
+        if (this.col == 1) return true;
+
+        int contador = 0;
+
+        for (int i = 0; i < this.lin; i++)
+        for (int j = 0; j < this.col; j++) 
+            if (matriz[j][i] != 0) {
+                contador++;
+                if (contador>1) return false;
+                break; 
+            }
+        
+        return true;
     }
 
     //10. Verificar se é uma matriz triangular inferior (só tem elementos da diagonal principal para baixo);
--- src/matrizesparsa/Teste.java
+++ src/matrizesparsa/Teste.java
@@ -4,7 +4,6 @@ public class Teste {
 
     //funcoes pra gerar as matrizes
     public static void main(String[] args) {
-
         int[][] m = {
             {0, 2, 0, 0, 0},
             {0, 0, 0, 0, 0},
@@ -12,21 +11,46 @@ public class Teste {
             {0, 0, 4, 3, 1},
             {5, 0, 0, 0, 0}
         };
+        int[][] m1 = {
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 4, 3, 1},
+            {0, 0, 0, 0, 0}
+        };
+        int[][] m2 = {
+            {0, 2, 0, 0, 0},
+            {0, 3, 0, 0, 0},
+            {0, 4, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0}
+        };
+        int[][] m3 = {
+            {5, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 3, 0},
+            {0, 0, 0, 0, 1}
+        };
 
         //1.1 testar classe matriz estatica
-        MatrizEstatica matriz = new MatrizEstatica(m);
+        MatrizEstatica matriz = new MatrizEstatica(m3);
 
-        matriz.insere(1, 1, 5);
-        matriz.remove(0, 4);
         matriz.imprime();
         //verifica se e linha coluna diagonal etc etc etc
+        System.out.println("É Matriz Diagonal? " + matriz.isDiagonal());
+        System.out.println("É Matriz Linha? "    + matriz.isLinha());
+        System.out.println("É Matriz Coluna? "   + matriz.isColuna());
+
+        //matriz.insere(1, 1, 5);
+        //matriz.remove(0, 4);
+        //matriz.imprime();
 
         //1.2 obter tempo de execucao
 
         //1.1 testar classe matriz dinamica
 
         //1.2 obter tempo de execucao
-        
     }
 }
 
